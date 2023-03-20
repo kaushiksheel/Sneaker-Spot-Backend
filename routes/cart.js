84fa3c3cbc314a90fccb2ee53a5a12bb1b3cbcf5
Devil5614737 <kaushiksheel9@gmail.com>
2023-03-20 19:28:26 +0530

    added checkout route

--- routes/cart.js
+++ routes/cart.js
@@ -1,3 +1,4 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const express = require("express");
 const router = express.Router();
 const auth = require("../middlewares/auth");
@@ -20,6 +21,51 @@ router.post("/add-to-cart", auth, async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(400).json(error);
+  }
+});
+
+router.post("/checkout", auth, async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      // customer_email: req.body.email,
+      submit_type: "pay",
+      billing_address_collection: "auto",
+      shipping_address_collection: {
+        allowed_countries: ["US", "CA"],
+      },
+      shipping_options: [{ shipping_rate: "shr_1M9N6vSEi48qcQQWQVHnuumn" }],
+      line_items: req.body.items.map((item) => {
+        const img = item.img;
+        return {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: item.name,
+              images: [img],
+            },
+            unit_amount: item.price * 100,
+          },
+          adjustable_quantity: {
+            enabled: true,
+            minimum: 1,
+            maximum: 5,
+          },
+          quantity: item.quantity,
+        };
+      }),
+
+      success_url: "http://127.0.0.1:5173/home",
+      cancel_url: "http://127.0.0.1:5173/cart",
+    });
+
+    res.json({ url: session.url });
+
+    // payment_success()
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ error: e.message });
   }
 });
 
